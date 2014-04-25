c84e4877c3ab2cc44bba8fcebf2d8031a9c14dea
menecio <aristobulo@gmail.com>
2014-04-25 11:11:23 -0430

    Add: web_hide_duplicate module, it allows you to hide 'Duplicate' action from 'Other' section by declaring duplicate="false" in <form>

---
+++ web_hide_duplicate/__openerp__.py
@@ -0,0 +1,8 @@
+{
+    'name': 'Hide duplicate',
+    'author': 'Aristobulo Meneses',
+    'description': 'Allows to hide duplicate button under <More> section',
+    'category': 'web',
+    'depends': ['web', ],
+    'js': ['static/src/js/duplicate.js', ],
+}
