652c77eba1a637ebc02733c10dff58da000d5625
menecio <aristobulo@gmail.com>
2014-05-09 19:47:27 -0430

    Removing unnecesary code.

--- web_hide_duplicate/static/src/js/duplicate.js
+++ web_hide_duplicate/static/src/js/duplicate.js
@@ -5,13 +5,11 @@ openerp.web_hide_duplicate = function (instance) {
         load_form: function(data) {
             this._super(data);
             // Now lets delete duplicate button from <More> section.
-            console.log('Deleting duplicate button/link');
             if (!this.is_action_enabled('duplicate')) {
                 var no_dup = _.reject(this.sidebar.items.other, function (item) {
                     return item.label === _t('Duplicate');
                 });
                 this.sidebar.items.other = no_dup;
-                this.sidebar.redraw();
             }
         }
     });
