a2fcca02793b8affe4e742f3c49fef441a0c3f99
menecio <aristobulo@gmail.com>
2014-05-20 14:00:02 -0430

    Addind logo and description to web_hide_duplicate module

--- web_hide_duplicate/__openerp__.py
+++ web_hide_duplicate/__openerp__.py
@@ -1,7 +1,18 @@
 {
     'name': 'Hide duplicate',
     'author': 'Aristobulo Meneses',
-    'description': 'Allows to hide duplicate button under <More> section',
+    'description': '''
+Allows to hide duplicate button under <More> section
+================================================================================
+
+HOW-TO:
+
+Go to desired form view definition and duplicate="false" attribute.
+
+Example:
+
+<form string="Users" version="7.0" duplicate="false">
+    ''',
     'category': 'web',
     'depends': ['web', ],
     'js': ['static/src/js/duplicate.js', ],
